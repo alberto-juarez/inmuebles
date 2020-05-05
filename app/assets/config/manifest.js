261c9498a9d9e60fdbce153c383a763a80d4809a
Alberto Juárez <aji147@gmail.com>
2020-05-05 15:16:10 -0500

    Ya jala

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,2 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link administrate/application.css
-//= link administrate/application.js
-//= link administrate-field-money/application.js
-//= link administrate-field-trix/application.css
-//= link administrate-field-trix/application.js
